c959dfb524eb5da3b272c964922ff5ed6568ee53
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-04-10 11:53:56 +0800

    refactor: update the frame code

--- new_version/cloudflare_bypass.py
+++ new_version/cloudflare_bypass.py
@@ -1,4 +1,5 @@
 from seleniumbase import SB
+import time
 
 def verify_success(sb):
     sb.assert_exact_text("我的E政府", "h1", timeout = 8)
@@ -12,8 +13,9 @@ with SB(uc_cdp=True, guest_mode=True) as sb:
     except Exception:
         if sb.assert_exact_text("www.gov.tw", "h1"):
             print("Click Again!")
-            # 確定不可行，需再定位，或換方式
-            sb.click('challenge-stage')
+            # 確定不可行，需再定位，或換方式 => 抓出來的 element #challenge-stage > div > label > input[type=checkbox]
+            sb.switch_to_frame('input[type=checkbox]')
+            sb.click("span.mark")
         try:
             verify_success(sb)
         except Exception:
