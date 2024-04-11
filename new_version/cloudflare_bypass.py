24473834fa8ae5a14aa1a5debbe65a2fc5e00f1f
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-04-11 14:38:34 +0800

    feat: Scrape the information from the first page

--- new_version/cloudflare_bypass.py
+++ new_version/cloudflare_bypass.py
@@ -1,5 +1,6 @@
 from seleniumbase import SB
-import time
+from selenium.webdriver.common.by import By
+import re
 
 def verify_success(sb):
     sb.assert_exact_text("我的E政府", "h1", timeout = 8)
@@ -12,13 +13,30 @@ with SB(uc_cdp=True, guest_mode=True) as sb:
         verify_success(sb)
     except Exception:
         if sb.assert_exact_text("www.gov.tw", "h1"):
+            # <iframe src="https://challenges.cloudflare.com/cdn-cgi/challenge-platform/h/b/turnstile/if/ov2/av0/rcv0/0/jqouz/0x4AAAAAAADnPIDROrmt1Wwj/dark/normal" allow="cross-origin-isolated; fullscreen" sandbox="allow-same-origin allow-scripts allow-popups" id="cf-chl-widget-jqouz" tabindex="0" title="包含 Cloudflare 安全性查問的小工具" style="border: none; overflow: hidden; width: 300px; height: 65px;"></iframe>
             print("Click Again!")
-            # 確定不可行，需再定位，或換方式 => 抓出來的 element #challenge-stage > div > label > input[type=checkbox]
-            sb.switch_to_frame('input[type=checkbox]')
+            sb.switch_to_frame('iframe[title="包含 Cloudflare 安全性查問的小工具"]')
             sb.click("span.mark")
         try:
+            sb.sleep(4)
             verify_success(sb)
         except Exception:
             raise Exception("Detected!")
-    for titles in driver.find_elements('td.td_title'):
-        print(titles.text)
+    all_subjects =  driver.find_elements('td.td_title')
+    all_organ =  driver.find_elements('td.td_organ')
+    for subject in all_subjects:
+        name = subject.text
+        # regex = re.compile('.*津貼.*|.*補助.*|.*給付.*|.*紓困.*|.*獎助學金.*|.*補貼.*')
+        match_result = re.match('.*津貼.*|.*補助.*|.*給付.*|.*紓困.*|.*獎助學金.*|.*補貼.*', name)
+        if match_result != None: # 符合上述關鍵字的標題才進入判斷
+            title_seq = all_subjects.index(subject)
+            organ = (all_organ[title_seq])
+            # 判定機關屬於中央還是地方政府
+            try:
+                organ_type = re.match('.*縣政府|.*市政府', organ.text).group()
+            except AttributeError:
+                organ_type = '中央政府'
+            
+            link = subject.find_element(By.TAG_NAME, 'a').get_attribute('href')
+
+            print(f'{name}$$${organ_type}$$$https://www.gov.tw/{link}')
