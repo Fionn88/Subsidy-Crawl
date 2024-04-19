ecd3175b2a1d8d45757e9afa0417c1c24b9bccf4
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-04-20 00:05:22 +0800

    feat: write data to sheet

--- new_version/example_sheet/write_to_sheet.py
+++ new_version/example_sheet/write_to_sheet.py
@@ -8,17 +8,11 @@ client = gspread.authorize(creds)
 sheet_id = "1-iWW4nYbc5Kx8WvAImKNBAXpYCy4mXj4k9SMOZ0R-nU"
 workbook = client.open_by_key(sheet_id)
 
+# sheet = workbook.worksheet("Values")
+# sheet.update_title("Hello World")
 
-value_list = workbook.sheet1.row_values(1)
-print(value_list)
-
-sheets = map(lambda x: x.title,workbook.worksheets())
-print(list(sheets))
+# worksheet = workbook.add_worksheet(title="A worksheet", rows=100, cols=20)
 
 sheet = workbook.worksheet("Hello World")
-value = sheet.acell("A1").value
-print(value)
-
-sheet = workbook.worksheet("Hello World")
-cell = sheet.find("Hello World This is Changed")
-print(cell.row, cell.col)
+# sheet.update_cell(1,1, "Hello World This is Changed")
+sheet.update_acell("A1", "Hello World This is Changed")
